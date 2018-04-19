ca437f724fc506b011aedd96248b87612b292ce7
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-04-19 17:47:39 +0100

    allow put axios

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
